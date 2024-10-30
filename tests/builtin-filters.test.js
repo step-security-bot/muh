891d199bbecf2418c25d2775c3608e7c9224e812
Lea Rosema <lea@lea.lgbt>
2024-10-30 23:07:21 +0100

    fix: fix tests

--- tests/builtin-filters.test.js
+++ tests/builtin-filters.test.js
@@ -1,69 +1,13 @@
-import { describe, it } from 'node:test'
-import assert from 'node:assert/strict';
-import path from 'node:path'
-import { handleTemplateFile } from '../src/transforms/template-data.js';
-import { SissiConfig } from '../src/sissi-config.js';
-describe('builtin filters', () => {
+import { describe, test } from 'node:test'
+import assert from 'node:assert/strict'
 
-  const dummyResolver = (map) => (...paths) => map.get(path.normalize(path.join(...paths)));
+import { json } from '../src/builtin-filters.js'
 
-  it('should format numbers', async () => {
-    const config = new SissiConfig();
-
-    const vFS = new Map();
-    vFS.set('index.html', '{{ thousandPi | numberFormat: numberFormatOptions, "de-DE" }}');
-
-    config.resolve = dummyResolver(vFS);
-
-    const data = { thousandPi: Math.PI * 1e3, numberFormatOptions: {maximumFractionDigits: 2, minimumFractionDigits: 2} };
-
-    const result = await handleTemplateFile(config, data, 'index.html');
-
-    assert.equal(result.content, '3.141,59');
+describe('built-in filters', () => {
+  test('json', () => {
+    assert.equal(json({name: 'Lea'}), '{"name":"Lea"}');
   });
 
-  it('should format currencies', async () => {
-    const config = new SissiConfig();
+  
 
-    const vFS = new Map();
-    vFS.set('index.html', '{{ million | currency: "eur", "de-DE" }}');
-
-    config.resolve = dummyResolver(vFS);
-
-    const data = { million: 1e6 };
-
-    const result = await handleTemplateFile(config, data, 'index.html');
-
-    assert.equal(result.content, '1.000.000,00 €');
-  });
-
-  it('should format dates', async () => {
-    const config = new SissiConfig();
-
-    const vFS = new Map();
-    vFS.set('index.html', '{{ newYear | date: dateFormatOptions, "de-DE" }}');
-
-    config.resolve = dummyResolver(vFS);
-
-    const data = { newYear: new Date('2025-04-01'), dateFormatOptions: {"day": "2-digit", "month": "2-digit", "year": "numeric"} };
-
-    const result = await handleTemplateFile(config, data, 'index.html');
-
-    assert.equal(result.content, '01.04.2025');
-  });
-
-  it('should serialize json', async () => {
-    const config = new SissiConfig();
-
-    const vFS = new Map();
-    vFS.set('index.html', '{{ answer | json }}');
-
-    config.resolve = dummyResolver(vFS);
-
-    const data = { answer: {result: 42} };
-
-    const result = await handleTemplateFile(config, data, 'index.html');
-
-    assert.equal(result.content, '{"result":42}');
-  });
 });
