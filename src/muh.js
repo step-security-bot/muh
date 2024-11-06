4522d294e9c30878167dab5a97f5758288b198a2
Lea Rosema <lea.rosema@adesso.de>
2024-11-06 17:44:43 +0100

    feat: create processTemplateFile function

--- src/muh.js
+++ src/muh.js
@@ -2,6 +2,8 @@ import vm from "node:vm";
 
 import * as builtinHelpers from "./builtin-helpers.js";
 import * as builtinFilters from "./builtin-filters.js";
+import { readFile } from "node:fs/promises";
+import path from "node:path";
 
 const TEMPLATE_REGEX = /\{\{\s*(.+?)\s*\}\}/gm;
 
@@ -51,19 +53,20 @@ export function parseFilterExpression(expr, ctx) {
 /**
  * @typedef TemplateConfig
  * Configuration object for the template() function.
- *
+ * @property {(file: string) => Promise<Buffer|String>} resolve
  * @property {Map<string, Function>} filters
- *
+ * @property {string} baseDir default: "."
+ * @property {string} includeDir default: "_includes" 
  */
 
 /**
  * Poor girl's handlebars
  *
- * @param {string} str the template content
+ * @param {string} content the template content
  * @param {TemplateConfig} config
  * @returns {Promise<string> => string} a function that takes a data object and returns the processed template
  */
-export async function template(str, data, config) {
+export async function template(content, data, config) {
   const defaultFilters = new Map();
   let isSafe = false;
   defaultFilters.set("safe", (input) => {
@@ -75,11 +78,11 @@ export async function template(str, data, config) {
   }
   const context = vm.createContext({ ...builtinHelpers, ...data });
   const filters = mergeMaps(
-    defaultFilters || new Map(),
-    config?.filters || new Map()
+    defaultFilters ?? new Map(),
+    config?.filters ?? new Map()
   );
   return (
-    await replaceAsync(str, TEMPLATE_REGEX, async (_, templateString) => {
+    await replaceAsync(content, TEMPLATE_REGEX, async (_, templateString) => {
       const expressions = templateString.split("|").map((e) => e.trim());
       const mainExpression = expressions[0];
       const filterExpressions = expressions.slice(1);
@@ -122,4 +125,56 @@ export async function template(str, data, config) {
       return isSafe ? result : htmlEscape(result);
     })
   ).replace(/\\([\{\}])/gm, "$1");
+}
+
+const defaultResolver = async (filePath) => {
+  if (/^https?:\/\//.test(filePath)) {
+    const response = await fetch(filePath);
+    if (response.status >= 400) {
+      throw new Error(response.statusText);
+    }
+    return await response.text() || '';
+  }
+  await readFile(filePath, 'utf-8') || '';
+}
+
+/**
+ * Process template file
+ * @param {string} inputContent file contents
+ * @param {string} inputFilePath file path
+ * @param {any} data template data
+ * @param {TemplateConfig} config 
+ * @returns 
+ */
+export async function processTemplateFile(inputContent, inputFilePath, data, config) {
+  const include = async (includeFilePath, dataOverrides) => {
+    const parentIncludes = config.parentIncludes?.slice(0) ?? [inputFilePath];
+    if (parentIncludes.includes(includeFilePath)) {
+      throw new Error('cyclic dependency detected.');
+    }
+    parentIncludes.push(includeFilePath);
+    const fullIncludePath = path.normalize(
+      path.join(
+        config?.baseDir ?? '.',
+        config?.includeDir ?? '_includes',
+        includeFilePath
+    ));
+    if (fullIncludePath.startsWith('..')) {
+      throw new Error(`invalid path "${fullIncludePath}": break out of the current working dir.`);
+    }
+    const includeContent = await (config.resolve ?? defaultResolver)(fullIncludePath);
+    return await processTemplateFile(
+      includeContent, 
+      includeFilePath, 
+      {
+        ...data, 
+        ...dataOverrides
+      }, {
+        ...(config ?? {}),
+        parentIncludes,
+      }
+    );
+  }
+  
+  return await template(inputContent, {...data, include}, config);
 }
