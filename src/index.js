1b3e3454a1ef54a8328a7a544eb3529e550501e0
Lea Rosema <lea@lea.lgbt>
2024-11-09 22:29:32 +0100

    feat: add smolyaml and frontmatter functions

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { template, handleTemplateFile } from './muh.js';
+import { smolYAML } from './smolyaml.js';
+import { frontmatter } from './frontmatter.js';
+
+export { template, handleTemplateFile, smolYAML, frontmatter };
+ 
