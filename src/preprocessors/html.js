a50adafbe299fe9f7826c988efe7ecdd79f83bee
Lea Rosema <lea@lea.codes>
2024-12-29 22:31:59 +0100

    chore: at least 1, at most 100 params

--- src/preprocessors/html.js
+++ src/preprocessors/html.js
@@ -6,7 +6,7 @@ export const htmlPreprocessor = {
   name: 'html',
   extension: /.html?$/,
   async process(content, data) {
-    return await replaceAsync(content, /<html-include((\s[a-z]{1,100}?=".{1,1024}?")*)>/gm,
+    return await replaceAsync(content, /<html-include((\s[a-z]{1,100}?=".{1,1024}?"){1,100})>/gm,
       async function (...matches) {
         const match = matches && matches.length >= 2 ? matches[1] : null;
         if (! match) {
